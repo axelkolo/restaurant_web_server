c221e4e71e48d992e1dcf67619cfcb074ae3db27
Ildevert Daisy <ildevertdaisy@gmail.com>
2023-01-30 15:07:28 +0100

    Commit nouvelle modification de index.js par rapport à la variable d'environnement PORT.

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ import workingHours from './data/workingHours.js';
 import menuItems from './data/menuItems.js';
 
 const app = express();
-const port = 3000;
+
+if(!process.env.PORT){
+    throw new Error("Impossible de lancer l'application. Veillez fournir le numéro de port via la variable d'environnement PORT s'il vous plaît");
+}
+
+
+const port = process.env.PORT;
+
 
 app.use(express.static("public"));
 
@@ -13,9 +20,6 @@ app.get("/", (req, res) => {
     res.render("index", { name: "Welcome to What Fare is Fair!" });
 });
 
-app.listen(port, () => {
-    console.log(`Web server is listening at localhost:${port}`);
-})
 
 app.get("/menu", (req, res) => {
     res.render("menu", { menuItems });
@@ -27,20 +31,22 @@ app.get("/about", (req, res) => {
 });
 
 // app.get("/hours", (req, res) => {
-//     res.render("hours", { workingHours });
-// });
-
-app.get('/hours', (req, res) => {
-    const days = [
-        "monday",
-        "tuesday",
-        "wednesday",
-        "thursday",
-        "friday",
-        "saturday",
-        "sunday",
-    ];
-    res.render("hours", { workingHours, days });
-});
-
-
+    //     res.render("hours", { workingHours });
+    // });
+    
+    app.get('/hours', (req, res) => {
+        const days = [
+            "monday",
+            "tuesday",
+            "wednesday",
+            "thursday",
+            "friday",
+            "saturday",
+            "sunday",
+        ];
+        res.render("hours", { workingHours, days });
+    });
+    
+app.listen(port, () => {
+        console.log(`Web server is listening at localhost:${port}`);
+    })
